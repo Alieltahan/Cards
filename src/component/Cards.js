816d8995e8462a048d000cb1420bc78569fb0223
Alieltahan <alyaltahan@gmail.com>
2021-11-15 19:25:20 +0200

    init

---
+++ src/component/Cards.js
@@ -0,0 +1,21 @@
+const Cards = () => {
+  return (
+    <>
+      <h1>Hello There</h1>
+      <div className="card__container">
+        <h4 className="card__name">
+          <div className="card__name__info">
+            <div className="card__name__info-item-1"></div>
+            <div className="card__name__info-item-2"></div>
+            <div className="card__name__info-item-3"></div>
+            <div className="card__name__info-item-4"></div>
+            <div className="card__name__info-item-5"></div>
+          </div>
+        </h4>
+      </div>
+      <p>Where Is It</p>
+    </>
+  );
+};
+
+export default Cards;
